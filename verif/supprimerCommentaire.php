6754f71c589a2a832c50c4d8b0beb93bc632ceec
AlanEc <alan.ecalle@laposte.net>
2017-05-24 18:12:08 +0200

    Modifs

--- verif/supprimerCommentaire.php
+++ verif/supprimerCommentaire.php
@@ -1,6 +1,11 @@
 <?php
 require('../../bootstrap.php');
 
+if (!isset($_SESSION['id'])) {
+    header('Location: ../controleurConnexion.php');
+    die();
+}
+
 if (isset($_GET['idCommentaire']) AND !empty($_GET['idCommentaire'])) {
     $id = $_GET['idCommentaire'];
     $commentaireManager->supprimer($id);
